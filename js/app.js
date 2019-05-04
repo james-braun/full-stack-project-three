14a472da3b216fc558d10fc4515f1b7a76f043e2
james-braun <braun.james.robert@gmail.com>
2019-05-03 19:26:28 -0500

    basicly works

--- js/app.js
+++ js/app.js
@@ -16,8 +16,7 @@ $('#title').change(function () {
     }
 });
 
-$('#design').change(function () { 
-        
+$('#design').change(function () {        
     if ($(this).val() === "js puns") {
         $('#colors-js-puns').show();
         $('#color').val('cornflowerblue');
@@ -34,8 +33,15 @@ $('#design').change(function () {
 });
 
 var number = 0;
+var checkBoxFlag = false;
 $('input[type="checkbox"]').each(function () {
     $(this).click(function () {
+        if (!checkBoxFlag) {
+            $('input[type="checkbox"]').each(function () {
+                $(this).parent().css('color', 'black');
+            });
+            checkBoxFlag = true;
+        }
 
         if ($(this).attr('name') === $('input[name="js-frameworks"]').attr('name')) {
             if ($(this).prop('checked')) {
@@ -90,14 +96,87 @@ $('input[type="checkbox"]').each(function () {
         }
         $('.activities p').remove();
         $('.activities').append('<p>Cost $' + number + '</p>');
-        //console.log($number);
     });
 });
 
+$('#payment').val("credit card");
+$('#credit-card + div').hide();
+$('#credit-card + div + div').hide();
+$('#payment').change(function () {
+    console.log($(this).val());
+    if ($(this).val() === "credit card") {
+        $('#credit-card').show();
+        $('#credit-card + div').hide();
+        $('#credit-card + div + div').hide();
+    } else if ($(this).val() === "paypal") {
+        $('#credit-card').hide();
+        $('#credit-card + div').show();
+        $('#credit-card + div + div').hide();
+    } else if ($(this).val() === "bitcoin") {
+        $('#credit-card').hide();
+        $('#credit-card + div').hide();
+        $('#credit-card + div + div').show();
+    }
+});
 
-//$('[name="express"]').prop("disabled", true).parent().css("color", "grey");
-//$('[name="express"]').prop("disabled", false).parent().css("color", "black");
+$('button').click(function (e) {
+    e.preventDefault();
+    if ($('#name').val() === '') {
+        $('#name').addClass("error");
+    } else {
+        $('#name').removeClass("error")
+    }
+    var regExpression = /[^@]+@[^@.]+\.[a-z]+/i;
+    if (regExpression.test($('#mail').val())) {
+        $('#mail').removeClass("error");
+    } else {
+        $('#mail').addClass("error");
+    }
+    checkBoxFlag = false;
+    $('input[type="checkbox"]').each(function () {
+        if ($(this).prop('checked')) {
+            checkBoxFlag = true;
+        }
+    });
+    if (!checkBoxFlag) {
+        $('input[type="checkbox"]').each(function () {
+            $(this).parent().css('color', 'red');
+        });
+    } else {
+        $('input[type="checkbox"]').each(function () {
+            $(this).parent().css('color', 'black');
+        });
+    }
+    if (!(/^[0-9]{13,16}$/.test($('#cc-num').val()))) {
+        $('#cc-num').addClass("error");
+    } else {
+        $('#cc-num').removeClass("error");
+    }
+    if (!(/^[0-9]{5,5}$/.test($('#zip').val()))) {
+        $('#zip').addClass("error");
+    } else {
+        $('#zip').removeClass("error");
+    }
+    if (!(/^[0-9]{3,3}$/.test($('#cvv').val()))) {
+        $('#cvv').addClass("error");
+    } else {
+        $('#cvv').removeClass("error");
+    }
+});
 
-//var number = parseInt($('[name="express"]').parent().text().split('$')[1]);
-//number += 1;
-//console.log(number);
+$('#name,#mail,#cc-num,#zip,#cvv').focus(function () {
+    $(this).removeClass("error");
+});
+
+$('label[for="mail"]').after('<span class="error-message"></span>');
+$('#mail').keyup(function () {
+    var regExpression = /[^@]+@[^@.]+\.[a-z]+/i;
+    if (regExpression.test($('#mail').val())) {
+        $('#mail').removeClass("error");
+        $('.error-message').text("");
+    } else {
+        $('#mail').addClass("error");
+        $('.error-message').text("Format: user@example.com");
+    }
+});
+
